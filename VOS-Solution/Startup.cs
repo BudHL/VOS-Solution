b356be7d984b79ba24e0049c4ce52cc4bfafaec9
BudHL <bud.long196@gmail.com>
2016-10-02 22:36:55 -0400

    Add project files.

---
+++ VOS-Solution/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VOS_Solution.Startup))]
+namespace VOS_Solution
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
